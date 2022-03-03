e0ddf06d15c2df7542c0d05448fac73a29190ace
yash <ywani@wisc.edu>
2022-03-03 11:56:17 -0600

    change internship -> full-time

--- src/Home.js
+++ src/Home.js
@@ -14,9 +14,8 @@ export default function Home() {
                         multiText={["Student", "Web Developer", "Machine Learning Enthusiast", "Undergraduate Researcher", "Intern", "Computer Scientist", "Electrical Engineer"]}
                         hideCursorAfterText={true} cursorColor={"white"} typeSpeed={50}/>
                     <p> Currently a student at the University of Wisconsin Madison, double majoring in Electrical
-                        Engineering and Computer Science. I am looking for Internship opportunities in the fields of
-                        Software
-                        Engineering, Electrical Engineering, and Computer Graphics.
+                        Engineering and Computer Science. I am looking for Full-time opportunities in the fields of
+                        Software Engineering, Electrical Engineering, and Computer Graphics.
                     </p>
                 </div>
                 <div className={"imgBox"}>
